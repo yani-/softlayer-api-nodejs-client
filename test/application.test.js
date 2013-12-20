62e635446af3a6354e8361c22d15bec18a187377
Yani Iliev <yani@servmask.com>
2013-12-20 07:28:16 +0700

    Removing unnecesary modules and adding the first test

---
+++ test/application.test.js
@@ -0,0 +1,24 @@
+/**
+ * SoftLayer API Node.js Client
+ *
+ * Copyright (c) 2013 Yani Iliev <yani@iliev.me>
+ * Licensed under the MIT license.
+ *
+ * @author    Yani Iliev <yani@iliev.me>
+ * @copyright 2013 Yani Iliev
+ * @license   https://github.com/yani-/blog/master/softlayer-api-nodejs-client
+ * @version   GIT: 1.0.0
+ * @link      https://github.com/yani-/softlayer-api-nodejs-client
+ */
+'use strict';
+
+var should    = require('chai').should();
+
+describe('App', function () {
+  describe('listen on passed port', function () {
+    it('should listen on port ', function (done) {
+      '123'.should.equal('123');
+      done();
+    });
+  });
+});
