74be5a0d86b7a6ca61ed7a84a76ba9a4e79c8da2
Yani Iliev <yani@servmask.com>
2013-12-20 21:12:59 +0700

    Adding more tests

--- test/application.test.js
+++ test/application.test.js
@@ -12,13 +12,42 @@
  */
 'use strict';
 
-var should    = require('chai').should();
+var should   = require('chai').should(),
+    slClient = null;
 
-describe('App', function () {
-  describe('listen on passed port', function () {
-    it('should listen on port ', function (done) {
-      '123'.should.equal('123');
+describe('SoftLayer API Node.js Client', function () {
+  describe('can be instantiated', function () {
+    it('should throw an exception when no valid args ', function (done) {
+      try {
+        slClient = require(__dirname + '/../index.js')();
+      } catch (e) {
+        (typeof e).should.equal('object');
+        e.message.should.equal('You need to provider api username and api key');
+        done();
+      }
+    });
+    it('should be instantiated via environment variables ', function (done) {
+      process.env.SOFTLAYERAPIUSERNAME = 'envUsername';
+      process.env.SOFTLAYERAPIKEY      = 'envKey';
+      slClient = require(__dirname + '/../index.js')();
+      slClient.username.should.equal(process.env.SOFTLAYERAPIUSERNAME);
+      slClient.key.should.equal(process.env.SOFTLAYERAPIKEY);
+      done();
+    });
+    it('should be instantiated via passed arguments ', function (done) {
+      var username = 'smapi',
+          key      = '873c47eef3eef3d188dd122391f953dab5d9a3ef69bde61cc49c26f' +
+                     'ebd9abc1e';
+      slClient = require(__dirname + '/../index.js')(username, key);
+      slClient.username.should.equal(username);
+      slClient.key.should.equal(key);
+      done();
+    });
+  });
+  describe('can make rest api calls', function () {
+    it('should ', function (done) {
       done();
     });
   });
 });
+// jN*@@FBCQZ3LuHJ@Gr%6
